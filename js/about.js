56e9b7f1e46251dae2990aca1f483163fd160d0a
lakshay-nasa <lakshay1717@gmail.com>
2023-01-30 03:10:23 +0530

    updated js

--- js/about.js
+++ js/about.js
@@ -4,7 +4,7 @@ unvlink.target = document.createTextNode("_blank")
 unvlink.innerHTML = "Deenbandhu Chhotu Ram University of Science and Technology, Murthal"
 
 const mypara = document.createElement("p");
-mypara.innerHTML = "My name is Lakshay and I am a junior pursuing my Bachelor's of Technology from India. I'm a Full Stack Web Developer and a DevOps Enthusiast. ";
+mypara.innerHTML = "Hello, My name is Lakshay. I am a pre-final year B.Tech student from DCRUST University, India. I am a Full Stack Developer and an Open Source Enthusiast. Presently, I’m working with Rennovation Software as a Software Engineer Intern. Besides this, I am also a Contributor at Data On Kubernetes Community to inspire folks to follow best practices over running data on Kubernetes.";
 // mypara.innerHTML = "Hi! I am Lakshay, a 3rd Year Electronics and Communication student at " + unvlink.outerHTML + ". I am a Full Stack Developer and an Open Source Enthusiast. Presently, I’m working with Rennovation Software as a Software Engineer Intern. Besides this, I am also a Contributor at Data On Kubernetes Community to inspire folks to follow best practices over running data on Kubernetes.";
 
 
